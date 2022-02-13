3e0c58f25203c4f99ee7b250da29b7d2511e5730
Debashish Das <hello@debashish.dev>
2022-02-13 15:08:45 +0600

    Dont Know What I have Done

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// shows #navbar element
+// function toggleCollapse('navbar');
+
+// hides #navbar element
+toggleCollapse('navbar', false);
+
+// also shows #navbar element
+toggleCollapse('navbar', true);
